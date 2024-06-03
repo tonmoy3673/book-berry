a5d0f903aa037a4499fa81a21c0c1ddc9da1e9c0
tonmoy <tonmoy3673@gmail.com>
2024-06-03 20:16:37 +0600

    :bulb: display books

--- src/components/HomePage/BooksItems/Books.jsx
+++ src/components/HomePage/BooksItems/Books.jsx
@@ -15,17 +15,17 @@ const breakpoints = {
     slidesPerView: 1,
   },
   768: {
-    slidesPerView: 2,
+    slidesPerView: 4,
   },
   1024: {
-    slidesPerView: 4,
+    slidesPerView: 5,
   },
 };
 const Books = ({ data }) => {
   return (
     <div className="pt-6 md:pt-10">
       <Swiper
-        spaceBetween={17}
+        // spaceBetween={5}
         modules={[Autoplay, Navigation]}
         breakpoints={breakpoints}
         loop={true}
@@ -38,25 +38,23 @@ const Books = ({ data }) => {
         speed={3000}
         allowTouchMove={false}
       >
-        {data?.map(({ title, image_url, price, _id }) => (
+        {data?.map(({ image_url, _id }) => (
           <SwiperSlide
             key={_id}
-            className={`text-center !ps-32 !md:ps-0  py-4 lg:py-10 `}
+            className={`text-center   py-16`}
           >
-            <div className="card card-compact  h-[260px] md:h-[320px] lg:h-[400px] w-60 lg:w-96 bg-base-100 shadow-xl">
-              <figure>
-                <img src={image_url} alt="Shoes" />
-              </figure>
-              <div className="card-body">
-                <h2 className="text-base md:text-xl text-center text-[#880769] font-semibold">
-                  {title}
-                </h2>
-                <p className="font-semibold text-red-600 text-base lg:text-lg">
-                  Price : {price}$
-                </p>
-                <div className="card-actions justify-center mt-2">
+            <div className="h-[240px] md:h-[264px] mx-auto">
+                  
+              
+                <img src={image_url} alt="books" className="!w-full  rounded-md !h-[240px] !md:h-[364px] object-contain" />
+               
+              
+              <div className="">
+              
+               
+                <div className="card-actions justify-center mt-4">
                   <Link to={`/books/${_id}`}>
-                    <button className="btn hover:bg-warning bg-[#880769] text-white">
+                    <button className="btn hover:bg-warning bg-[#00897B] text-white">
                       Details
                     </button>{" "}
                   </Link>
--- src/components/HomePage/BooksItems/SwiperSliderButton.jsx
+++ src/components/HomePage/BooksItems/SwiperSliderButton.jsx
@@ -14,25 +14,25 @@ export const SwiperNavButtons = () => {
       <>
         <div className="flex lg:gap-8 gap-3 items-center justify-center lg:py-6 py-3">
           {/* ========= first line ======== */}
-          <div className="lg:w-[330px] w-[220px] h-0.5 bg-[#880769]">
+          <div className="lg:w-[330px] w-[220px] h-0.5 bg-[#00897B]">
 
           </div>
           <div className="flex lg:gap-5 gap-3">
             <button
               onClick={() => swiper.slidePrev()}
-              className="p-3 rounded-full border-2 border-[#880769] hover:text-white hover:bg-[#880769]"
+              className="p-3 rounded-full border-2 border-[#00897B] hover:text-white hover:bg-[#00897B]"
             >
-              <FaChevronLeft className="fill-[#880769] hover:fill-white"/>
+              <FaChevronLeft className="fill-[#00897B] hover:fill-white"/>
             </button>
             <button
               onClick={() => swiper.slideNext()}
-              className="hover:text-white bg-[#880769]  border-2 p-3  rounded-full hover:bg-white border-[#880769]"
+              className="hover:text-white bg-[#00897B]  border-2 p-3  rounded-full hover:bg-white border-[#00897B]"
             >
-              <FaChevronRight className='hover:fill-[#880769] fill-white'/>
+              <FaChevronRight className='hover:fill-[#00897B] fill-white'/>
             </button>
           </div>
           {/* =============== second line ============= */}
-          <div className="lg:w-[330px] w-[220px] h-0.5 bg-[#880769]"></div>
+          <div className="lg:w-[330px] w-[220px] h-0.5 bg-[#00897B]"></div>
         </div>
       </>
     </div>
