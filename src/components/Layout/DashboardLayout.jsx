30f6f6188b6815250ee9294ad5659394b76d2a74
tonmoy <tonmoy3673@gmail.com>
2024-06-21 16:01:09 +0600

    :bulb: set dashboard

--- src/components/Layout/DashboardLayout.jsx
+++ src/components/Layout/DashboardLayout.jsx
@@ -1,33 +1,51 @@
+import { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
+import AdminNavbar from "../Dashboard/AdminNavbar";
+import Sidebar from "../Dashboard/Sidebar";
+import useMediaQuery from "../utilities/useMediaQuery";
 
-import { Link, Outlet} from "react-router-dom";
 
-const DashboardLayout = () => {
+const DashboardLayout=()=> {
+  const isBetweenMdAndLg = useMediaQuery('(min-width: 1024px) and (max-width: 1300px)');
+  const [display, setDisplay] = useState(false)
+  const [minimize, setMinimize] = useState(false)
+
+  const handleMenu = () =>{
+    setDisplay(!display)
+  }
+
+  const handleMinimizeSidebar = () =>[
+    setMinimize(!minimize)
+  ]
+
+    
+  useEffect(() => {
+    if (isBetweenMdAndLg) {
+      setMinimize(true);
+    }else {
+      setMinimize(false);
+    }
+  }, [isBetweenMdAndLg]);
 
   return (
-    <div className="grid grid-cols-12">
-      <div className="col-span-2 bg-base-300 min-h-screen p-3 lg:p-12 pt-12 lg:pt-28">
-        <ul>
-        <Link to="/dashboard/user"><button className="bg-[#00897B] hover:bg-warning rounded-md mb-6 p-4 text-white w-full">
-        User Info
-            </button></Link>
-          
-            <Link to="/dashboard/all-products"><button className="bg-[#00897B] hover:bg-warning rounded-md mb-6 p-4 text-white w-full">
-            All Products
-            </button></Link>
-            <Link to="/dashboard/add-products"><button className="bg-[#00897B] hover:bg-warning rounded-md mb-6 p-4 text-white w-full">
-            Add Product
-           </button></Link>
-           <Link to="/" ><button className="bg-[#00897B] hover:bg-warning rounded-md p-4 text-white w-full">
-           HomePage
-           </button></Link>
-          
-        </ul>
-      </div>
-      <div className="col-span-10 p-10 md:p-20">
-        <Outlet />
+    <div className="flex flex-col h-screen bg-slate-800 ">
+      <AdminNavbar
+      handleMenu={handleMenu} 
+      display={display}
+      handleMinimize={handleMinimizeSidebar}
+      minimize={minimize}
+      />
+      <div className="flex flex-1 overflow-hidden">
+        <Sidebar
+        display={display}
+        minimize={minimize}
+        />
+        <main className="flex-1 sm:rounded-tl-lg lg:rounded-tl-lg dash-bg bg-slate-700 text-white p-5">
+          <Outlet/>
+        </main>
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default DashboardLayout;
