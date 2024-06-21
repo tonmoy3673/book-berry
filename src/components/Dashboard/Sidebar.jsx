ca005041a2806ab760179cb5e10ea51f30236ba8
tonmoy <tonmoy3673@gmail.com>
2024-06-21 16:30:08 +0600

    :lipstick: added CSS

--- src/components/Dashboard/Sidebar.jsx
+++ src/components/Dashboard/Sidebar.jsx
@@ -1,8 +1,7 @@
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import HomeIcon from "@mui/icons-material/Home";
-import LogoutIcon from "@mui/icons-material/Logout";
 import MenuBookIcon from "@mui/icons-material/MenuBook";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { BiUserCheck } from "react-icons/bi";
 import { Link } from "react-router-dom";
 
 const Sidebar = ({ display, minimize }) => {
@@ -45,7 +44,7 @@ const Sidebar = ({ display, minimize }) => {
               </div>
             </li>
           </Link>
-          <Link to="/dashboard/books">
+          <Link to="/dashboard/all-products">
             <li className="flex items-center group  w-full rounded cursor-pointer transition-all duration-[400ms] relative overflow-hidden py-2 px-4 bg-white hover:bg-white">
               <MenuBookIcon />
               <div
@@ -75,38 +74,22 @@ const Sidebar = ({ display, minimize }) => {
             </li>
           </Link>
 
-          <Link to="/dashboard/orders">
+          <Link to="/dashboard/user">
             <li className="flex items-center group  w-full rounded cursor-pointer transition-all duration-[400ms] relative overflow-hidden py-2 px-4 bg-white hover:bg-white">
-              <ShoppingCartIcon />
+              <BiUserCheck />
               <div
                 className={`${
                   minimize ? "opacity-0 w-full" : "opacity-100 w-full"
                 }  flex items-center transition-all duration-[400ms] ease-linear flex-nowrap justify-between`}
               >
                 <p className="capitalize whitespace-nowrap ml-[13px] opacity-100 transition-opacity duration-150 ease-linear">
-                  Orders
+                  Users
                 </p>
               </div>
             </li>
           </Link>
         </div>
-        <div className="py-6 px-4">
-          <Link
-            to="#"
-            className="flex items-center group  w-full rounded cursor-pointer transition-all duration-[400ms] relative overflow-hidden py-2 px-4 bg-white hover:bg-white"
-          >
-            <LogoutIcon />
-            <div
-              className={`${
-                minimize ? "opacity-0 w-full" : "opacity-100 w-full"
-              } flex items-center transition-all duration-[400ms] ease-linear flex-nowrap justify-between`}
-            >
-              <p className="capitalize whitespace-nowrap ml-[13px] opacity-100 transition-opacity duration-150 ease-linear">
-                Logout
-              </p>
-            </div>
-          </Link>
-        </div>
+        
       </nav>
     </aside>
   );
